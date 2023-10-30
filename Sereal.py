e60bd2fd58fe86333638da6089157cc19203d1d0
inizarr <149244950+inizarr@users.noreply.github.com>
2023-10-30 08:12:36 +0700

    Add files via upload

---
+++ Sereal.py
@@ -0,0 +1,23 @@
+import pickle
+import streamlit as st
+
+# Muat model
+model = pickle.load(open('Sereal.sav', 'rb'))
+
+st.title('Estimasi Sereal')
+
+calories = st.number_input('Input calories')
+protein = st.number_input('Input protein')
+fat = st.number_input('Input fat')
+sodium = st.number_input('Input sodium')
+fiber = st.number_input('Input fiber')
+carbo = st.number_input('Input carbo')
+sugars = st.number_input('Input sugars')
+rating = st.number_input('Input rating')
+
+predict = ''
+
+if st.button('Estimasi Sereal'):
+    input_data = [[calories, protein, fat, sodium, fiber, carbo, sugars, rating]]
+    predict = model.predict(input_data)
+    st.write('Estimasi Sereal:', predict[0])
